13e89718202f794cc044f7990b716a62e9aa8384
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-11-15 15:26:47 -0700

    initial commit

---
+++ link-tree/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+//component imports
+import Topper from "./Components/Topper";
+
+function App() {
+  return (
+    <div className="App">
+      <Topper />
+    </div>
+  );
+}
+
+export default App;
